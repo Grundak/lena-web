f164402ee633c0b0be0bafc63002c81896c99329
Grundak <davca.grund@gmail.com>
2025-04-09 18:44:37 +0200

    Init navigation component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="navigation">
+          <div className="nav-left">
+            <a href="#lena">Lena</a>
+            <a href="#galerie">Galerie</a>
+            <a href="#kontakt">Kontakt</a>
+          </div>
+          <div className="nav-right">
+            <span>Lena Tattoo Art</span>
+            <img src={logo} alt="Logo" className="logo" />
+          </div>
+        </div>
       </header>
     </div>
   );
