b21a895c9f8afa43539b0517ad2cae3a8ae7ae20
Grundak <davca.grund@gmail.com>
2025-04-09 19:36:44 +0200

    Add reset syles, basic and navi, fixed navigation

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ function App() {
       <header className="App-header">
         <div className="navigation">
           <div className="nav-left">
-            <a href="#lena">Lena</a>
-            <a href="#galerie">Galerie</a>
-            <a href="#kontakt">Kontakt</a>
-          </div>
-          <div className="nav-right">
             <span>Lena Tattoo Art</span>
             <img src={logo} alt="Logo" className="logo" />
+          </div>
+          <div className="nav-right">
+            <ol>
+              <li><a href="#lena">Lena</a></li>
+              <li><a href="#galerie">Galerie</a></li>
+              <li><a href="#kontakt">Kontakt</a></li>
+            </ol>
           </div>
         </div>
       </header>
