f5906e39963671e9a868510c3367d9bec6b87296
Grundak <davca.grund@gmail.com>
2025-04-09 19:44:11 +0200

    Add Lena plus basic css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import lenaImage from './lena.jpg'; // Přidejte obrázek Leny do projektu a importujte jej
 import './App.css';
 
 function App() {
@@ -19,6 +20,22 @@ function App() {
           </div>
         </div>
       </header>
+      <section id="lena" className="lena-section">
+        <div className="lena-left">
+          <h2>Lena Tattoo Art</h2>
+          <img src={lenaImage} alt="Lena" className="lena-image" />
+        </div>
+        <div className="lena-right">
+          <h3>O mně</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Vivamus lacinia odio vitae vestibulum vestibulum. 
+            Cras venenatis euismod malesuada. 
+            Nulla facilisi. 
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
